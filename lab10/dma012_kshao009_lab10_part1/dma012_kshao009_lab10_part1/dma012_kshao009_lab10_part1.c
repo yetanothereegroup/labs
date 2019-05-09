22bbc66caa4ce64a7dd075f85bcf798f47a16d64
Daniel Ma <daniel.r.ma@gmail.com>
2019-05-09 08:52:25 -0700

    Lab 10 part 4 done (period 2,3,4,5,...,127ms).

--- lab10/dma012_kshao009_lab10_part1/dma012_kshao009_lab10_part1/dma012_kshao009_lab10_part1.c
+++ lab10/dma012_kshao009_lab10_part1/dma012_kshao009_lab10_part1/dma012_kshao009_lab10_part1.c
@@ -37,15 +37,19 @@ typedef struct _task {
     uint32_t (*tick)(uint32_t);
 } task;
 
-#define TASK_SIZE 4
+#define TASK_SIZE 5
 task task_list[TASK_SIZE];
 
 
 enum _state_TLED {S_TLED_1, S_TLED_2, S_TLED_3};
 enum _state_BLINK {S_BLINK_ON, S_BLINK_OFF};
 enum _state_BUZZER {S_B4_ON, S_B4_OFF};
+enum _state_FREQSET {S_FREQSET_IDLE, S_FREQSET_UP, S_FREQSET_UP_WAIT, S_FREQSET_DOWN, S_FREQSET_DOWN_WAIT};
     
 uint8_t B0, B1, B2, B3, B4;
+uint8_t A0, A1, A2, A3, A4;
+
+uint8_t frequency = 10; // Not actual frequency in Hz.  Length of period in ms.
     
 uint32_t tick_TLED (uint32_t state) {
     // Handle state transitions
@@ -112,6 +116,14 @@ uint32_t tick_OUTPUT(uint32_t unused) {
     SET_BIT(b_buf, 3, B3);
     SET_BIT(b_buf, 4, B4);
     
+    uint8_t a_buf = PINA;
+    
+    A0 = (GET_BIT(a_buf, 0)) ? 0 : 1;
+    A1 = (GET_BIT(a_buf, 1)) ? 0 : 1;
+    A2 = (GET_BIT(a_buf, 2)) ? 0 : 1;
+    A3 = (GET_BIT(a_buf, 3)) ? 0 : 1;
+    A4 = (GET_BIT(a_buf, 4)) ? 0 : 1;
+    
     
     
     PORTB = b_buf;
@@ -121,12 +133,18 @@ uint32_t tick_OUTPUT(uint32_t unused) {
 }
 
 uint32_t tick_BUZZER(uint32_t state) {
+    static uint8_t cnt;
     switch(state) {
         case S_B4_ON:
-        state = S_B4_OFF;
+        cnt++;
+        if (cnt >= frequency / 2) state = S_B4_OFF;
         break;
         case S_B4_OFF:
-        state = S_B4_ON;
+        cnt++;
+        if (cnt >= frequency) {
+            state = S_B4_ON;
+            cnt = 0;
+        }            
         break;
         
         default:
@@ -140,6 +158,53 @@ uint32_t tick_BUZZER(uint32_t state) {
         break;
         case S_B4_OFF:
         B4 = 0;
+        break;
+    }
+    
+    return state;
+}
+
+uint32_t tick_FREQSET(uint32_t state) {
+    switch(state) {
+        case S_FREQSET_IDLE:
+        if (A0) state = S_FREQSET_UP;
+        else if (A1) state = S_FREQSET_DOWN;
+        break;
+        
+        case S_FREQSET_UP:
+        state = S_FREQSET_UP_WAIT;
+        break;
+        
+        case S_FREQSET_UP_WAIT:
+        if (!A0) state = S_FREQSET_IDLE;
+        break;
+        
+        case S_FREQSET_DOWN:
+        state = S_FREQSET_DOWN_WAIT;
+        break;
+        case S_FREQSET_DOWN_WAIT:
+        if (!A1) state = S_FREQSET_IDLE;
+        break;
+        default:
+        state = S_FREQSET_IDLE;
+        break;
+        
+    }
+    
+    switch(state) {
+        case S_FREQSET_IDLE:
+        break;
+        case S_FREQSET_UP:
+        if (frequency < 127) frequency++;
+        break;
+        case S_FREQSET_UP_WAIT:
+        break;
+        case S_FREQSET_DOWN:
+        if (frequency > 0) frequency--;
+        break;
+        case S_FREQSET_DOWN_WAIT:
+        break;
+        default:
         break;
     }
     
@@ -173,14 +238,19 @@ int main(void) {
     task_list[1].tick = &tick_BLINK;
     
     task_list[2].elapsed_time = 0;
-    task_list[2].period = 2;
+    task_list[2].period = 1;
     task_list[2].state = 0;
     task_list[2].tick = &tick_OUTPUT;
     
     task_list[3].elapsed_time = 0;
-    task_list[3].period = 2;
+    task_list[3].period = 1;
     task_list[3].state = S_B4_ON;
     task_list[3].tick = &tick_BUZZER;
+    
+    task_list[4].elapsed_time = 0;
+    task_list[4].period = 1;
+    task_list[4].state = S_FREQSET_IDLE;
+    task_list[4].tick = &tick_FREQSET;
     
     
     timer_init();
